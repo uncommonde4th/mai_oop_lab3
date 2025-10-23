ba00e07b50be7506c29cb5f06d906c42cc9575a5
George Brezhnev <uncommonde4th@gmail.com>
2025-10-23 20:49:19 +0300

    v0.4

--- include/figure.h
+++ include/figure.h
@@ -7,5 +7,26 @@ class Figure {
     public:
         Figure(unsigned int numOfPoints = 4);
         ~Figure();
-}
-    
+        
+        Figure& operator=(const Figure& other);
+        Figure& operator=(Figure&& other);
+        virtual bool operator==(const Figure& other) const =0;
+        
+        Point getCenter() const;
+        unsigned int getNumOfPoints() const;
+        Point* getPoints() const;
+
+        virtual double getArea() const = 0;
+        explicit operator double() const { return getArea(); }
+        virtual void print(std::ostream& os) const = 0;
+        virtual void read(std::ostream& is) = 0;
+
+        friend std::ostream&operator<<(std::ostream& os, const Figure& other);
+        friend std::ostream&operator>>(std::ostream& is, Figure& other);
+
+    protected:
+        unsigned int numOfPoints;
+        Point *points;
+};
+
+#endif
--- src/figure.cpp
+++ src/figure.cpp
@@ -10,3 +10,60 @@ Figure::~Figure() {
     numOfPoints = 0;
 }
 
+Figure& Figure::operator=(const Figure& other) {
+    if (this != &other) {
+        delete[] points;
+        numOfPoints = other.numOfPoints;
+        if (other.numOfPoints > 0) {
+            points = new Point[numOfPoints];
+            for (unsigned int i = 0; i < numOfPoints; i++) {
+                points[i] = other.points[i];
+            }
+        }
+    }
+
+    return *this;
+}
+
+Figure& Figure::operator=(Figure&& other) {
+    if (this != &other) {
+        delete[] points;
+        numOfPoints = other.numOfPoints;
+        points = other.points;
+
+        other.numOfPoints = 0;
+        other.points = nullptr;
+    }
+
+    return *this;
+}
+
+Point Figure::getCenter() const {
+    double xc = 0, yc = 0;
+    for (unsigned int i = 0; i < numOfPoints; i++) {
+        xc += points[i].x;
+        yc += points[i].y;
+    }
+    xc /= numOfPoints;
+    yc /= numOfPoints;
+
+    return Point(xc, yc);
+}
+
+unsigned int Figure::getNumOfPoints() const {
+    return numOfPoints;
+}
+
+Point* Figure::getPoints() const {
+    return points;
+}
+
+std::ostream& operator<<(std::ostream& os, const Figure& figure) {
+    figure.print(os);
+    return os;
+}
+
+std::ostream& operator>>(std::ostream& is, Figure& figure) {
+    figure.read(is);
+    return is;
+}
